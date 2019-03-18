3468b5a526e89279def847259fe36fec0ac9b515
rafal_ubuntu_praca <rafal@gmail>
2019-03-18 08:58:28 +0100

    log

--- src/main/java/asystent_echo/ActionEchoService.java
+++ src/main/java/asystent_echo/ActionEchoService.java
@@ -1,9 +1,16 @@
 package asystent_echo;
 
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
 public class ActionEchoService {
 
 	
 	public String echoRespone(String jsonString) {
+		log.info("*********Echo service: " + jsonString);
 		return "echo backend";
 	}
 }
